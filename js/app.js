1f30c253116b325569c1aa805ee8105a68d71937
mac <mac@macs-MacBook-Pro.local>
2018-06-27 16:51:55 -0600

    Commit 5: added more logic and styling

--- js/app.js
+++ js/app.js
@@ -1,4 +1,4 @@
-console.log("linked");
+//console.log("linked");
 
 //1. create a Class (JS class)
 $(document).ready(function() {
@@ -6,26 +6,27 @@ $(document).ready(function() {
 
 class tamagotchi {
 	constructor (hunger, boredom, sleepiness, age){
-		this.hunger = 10;
-		this.boredom = 10;
-		this.sleepiness = 10;
-		this.age = 1
+		this.hunger = hunger;
+		this.boredom = boredom;
+		this.sleepiness = sleepiness;
+		this.age = age;
 		
 	}
 	eat() {
 			if (this.hunger < 10){
 				this.hunger++;
-				console.log("Finally!, I'm starving over here!");
+				console.log("Finally! I'm starving over here!");
 		} else {
 			alert("Fool!! Evil Lincoln does not require food!")
 		};
+
 	}	
 	sleep() {
 			if (this.sleepiness < 10){
 				this.sleepiness++;
 				console.log("Yo! I'm sleepy y'all!");
 		} else {
-			alert("Wide awake bitch!");
+			alert("Wide awake, bitch!");
 		}
 	}	
 	play() {
@@ -42,32 +43,51 @@ let evilLincoln = new tamagotchi(10, 10, 10, 1);
 let seconds = 0;
 
 const timePasses = () => {
-			//console.log(`It has been ${seconds} seconds`)
+	//console.log(`It has been ${seconds} seconds`)
 	seconds++;
-	evilLincoln.hunger--;
-	evilLincoln.sleepiness--;
-	evilLincoln.boredom--;
-	evilLincoln.age++;
+	// evilLincoln.hunger--;
+	// evilLincoln.sleepiness--;
+	// evilLincoln.boredom--;
+	// evilLincoln.age++;
+
+	if(seconds % 2 == 0){
+		evilLincoln.hunger--;
+	}
+
+	if(seconds % 5 == 0){
+		evilLincoln.boredom--;
+	}
+	if(seconds % 10 == 0){
+		evilLincoln.sleepiness--;
+	}
+	if(seconds % 1 == 0){
+		evilLincoln.age++;
+	}
 
 		console.log("Sleepiness " + evilLincoln.sleepiness)
 		console.log("Hunger " + evilLincoln.hunger)
 		console.log("Boredom " + evilLincoln.boredom)
 		console.log("Age " + evilLincoln.age)
 
+	 $('#food-bar').css("width", (evilLincoln.hunger * 10)+ "%");
+	 $('#sleep-bar').css("width", (evilLincoln.sleepiness * 10)+ "%");
+	 $('#boredome-bar').css("width", (evilLincoln.boredom * 10)+ "%");
+	 $('#age-bar').css("width", (evilLincoln.age * 10)+ "%");
+
 	if(evilLincoln.hunger == 0|| evilLincoln.boredom == 0 || evilLincoln.sleepiness == 0){
 		alert("EVIL LINCOLN HAS DIED!!")
 		
 		clearInterval(timePassing);
 
-	$("#foody").empty().append(evilLincoln.hunger);
-	// $('.').empty().append(charlie.hunger);
-	// $('.hungry').empty().append(charlie.hunger);
-	// $('.hungry').empty().append(charlie.hunger);
+	//$("#").empty().append(evilLincoln.hunger);
+	// $('.sleepiness').empty().append(evilLincoln.sleepiness);
+	// $('.boredom').empty().append(evilLincoln.boredom);
+	// $('.age').empty().append(evilLincoln.age);
 		}
 		
 	}
 
-const timePassing = setInterval(timePasses, 5000);
+const timePassing = setInterval(timePasses, 1000);
 
 $("#set-name").on("click", () => {
 
@@ -79,8 +99,6 @@ $("#set-name").on("click", () => {
 	})
 
 $("#food").on("click", () => {
-	let health = document.getElementById("foody")
-		health.value += 1;
 
 		evilLincoln.eat();
 
@@ -88,8 +106,6 @@ $("#food").on("click", () => {
 })
 
 $("#sleepytime").on("click", () => {
-	let health = document.getElementById("sleepy")
-		health.value += 1;
 
 		evilLincoln.sleep();
 
@@ -98,38 +114,18 @@ $("#sleepytime").on("click", () => {
 })
 
 $("#play").on("click", () => {
-	let health = document.getElementById("bored")
-		health.value += 1;
 
 		evilLincoln.play();
 
-	//console.log(evilLincoln.boredom)
 	//console.log("Iron helps us play!")
 })
 
-const checkForUpdate = () => {
-
-}
-
 
 });
-//2. Instantiate the tamagotchi
 
-//3. Display character of choice on the screen to represent pet
+
 
 //4. Display following metrics; Hunger(1-10 scale), Sleepiness(1-10), Boredom(1-10), Age.
-
-//5. Add buttons to screen to feed pet, make it sleep, and play with it.
-
-//6. Add ability to name the pet.
-
-//7. Style the page.
-
-//8. Increase pet's age every X minutes.
-
-//9. Increase the pet's Hunger, Boredom, and Sleepiness at interval of your choosing.
-
-//10. Pet should die if any of the metrics hit 10.
 
 //11. Morph pet at certain ages.
 
